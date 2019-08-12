48b9da4eeae889ab0240bcd86c3470595af07f64
Jeffrey R. Carter <jrcarter@acm.org>
2019-08-12 22:20:19 +0200

    Add Base to Float_Image

--- pragmarc-images.adb
+++ pragmarc-images.adb
@@ -1,13 +1,18 @@
 -- PragmAda Reusable Component (PragmARC)
--- Copyright (C) 2018 by PragmAda Software Engineering.  All rights reserved.
+-- Copyright (C) 2019 by PragmAda Software Engineering.  All rights reserved.
 -- **************************************************************************
 --
 -- History:
+-- 2019 Aug 15     J. Carter          V1.3--Added Base to Float_Image
 -- 2018 Aug 01     J. Carter          V1.2--Cleanup compiler warnings
 -- 2006 Mar 01     J. Carter          V1.1--Added Float_Image
 -- 2004 Apr 01     J. Carter          V1.0--Initial version
 --
 with Ada.Strings.Fixed;
+with Ada.Strings.Unbounded;
+with PragmARC.Unbounded_Conversions;
+with PragmARC.Unbounded_Integers;
+
 package body PragmARC.Images is
    use Ada.Strings.Fixed;
    use Ada.Text_IO;
@@ -86,23 +91,83 @@ package body PragmARC.Images is
       return Adjust (Image (Start .. Stop), Width, False, Zero_Filled);
    end Modular_Image;
 
-   function Float_Image
-      (Value : Number; Fore : Field := 2; Aft : Field := Number'digits - 1; Exp : Field := 3; Zero_Filled : Boolean := False)
+   function Float_Image (Value       : Number;
+                         Fore        : Field       := 2;
+                         Aft         : Field       := Number'Digits - 1;
+                         Exp         : Field       := 3;
+                         Zero_Filled : Boolean     := False;
+                         Base        : Number_Base := 10)
    return String is
       package Number_IO is new Float_IO (Number);
 
-      Image : String (1 .. 3 * Field'Last + 3);
-      Start : Natural;
-      Width : Field := Fore + Aft + 1;
-   begin -- Float_Image
-      Number_IO.Put (To => Image, Item => abs Value, Aft => Aft, Exp => Exp);
-      Start := Index_Non_Blank (Image);
+      use Ada.Strings.Unbounded;
+      use Unbounded_Conversions;
 
-      if Exp > 0 then
-         Width := Width + Exp + 1;
+      function Base_10_Image return String is
+         Image : String (1 .. 3 * Field'Last + 3);
+         Start : Natural;
+         Width : Field := Fore + Aft + 1;
+      begin -- Base_10_Image
+         Number_IO.Put (To => Image, Item => abs Value, Aft => Aft, Exp => Exp);
+         Start := Index_Non_Blank (Image);
+
+         if Exp > 0 then
+            Width := Width + Exp + 1;
+         end if;
+
+         return Adjust (Image (Start .. Image'Last), Width, Value < 0.0, Zero_Filled);
+      end Base_10_Image;
+
+      function Non_10_Image return String is
+         function Hex_Digit (Value : Natural) return Character is
+         begin -- Hex_Digit
+            if Value < 10 then
+               return Character'Val (Character'Pos ('0') + Value);
+            end if;
+
+            return Character'Val (Character'Pos ('A') + Value - 10);
+         end Hex_Digit;
+
+         Work  : Number := abs Value;
+         Image : String (1 .. 12 * Field'Last + 3);
+         Start : Natural;
+         Dot   : Natural;
+         Col   : Number := 1.0 / Number (Base);
+         Digit : Natural;
+      begin -- Non_10_Image
+         Number_IO.Put (To => Image, Item => Work, Aft => Aft, Exp => Exp);
+         Start := Index_Non_Blank (Image);
+         Dot := Index (Image, ".");
+
+         Integer_Part : declare
+            Full_Image : Unbounded_String :=
+               +Unbounded_Integers.Image (Unbounded_Integers.Value (Image (Start .. Dot - 1) ),
+                                          Base => Unbounded_Integers.Base_Number (Base) ) & '.';
+         begin -- Integer_Part
+            Work := Work - Number'Value (Image (Start .. Dot) & '0');
+
+            Extract_Fraction : for K in 1 .. 1_000 Loop
+               exit Extract_Fraction when Work <= 0.0;
+
+               Digit := Integer (Number'Truncation (Work / Col) );
+               Append (Source => Full_Image, New_Item => Hex_Digit (Digit) );
+               Work := Work - Number (Digit) * Col;
+               Col := Col / Number (Base);
+            end loop Extract_Fraction;
+
+            if Element (Full_Image, Length (Full_Image) ) = '.' then
+               Append (Source => Full_Image, New_Item => '0');
+            end if;
+
+            return Adjust (+Full_Image, Fore + Aft + 1, Value < 0.0, Zero_Filled);
+         end Integer_Part;
+      end Non_10_Image;
+   begin -- Float_Image
+      if Base = 10 then
+         return Base_10_Image;
       end if;
 
-      return Adjust (Image (Start .. Image'Last), Width, Value < 0.0, Zero_Filled);
+      return Non_10_Image;
    end Float_Image;
 end PragmARC.Images;
 --
--- pragmarc-images.ads
+++ pragmarc-images.ads
@@ -1,19 +1,21 @@
 -- PragmAda Reusable Component (PragmARC)
--- Copyright (C) 2006 by PragmAda Software Engineering.  All rights reserved.
+-- Copyright (C) 2019 by PragmAda Software Engineering.  All rights reserved.
 -- **************************************************************************
 --
 -- Extended image functions for integer types
 --
 -- History:
+-- 2019 Aug 15     J. Carter          V1.2--Added Base to Float_Image
 -- 2006 Mar 01     J. Carter          V1.1--Added Float_Image
 -- 2004 Apr 01     J. Carter          V1.0--Initial version
 --
 with Ada.Text_IO;
+
 package PragmARC.Images is
    subtype Field       is Ada.Text_IO.Field;
    subtype Number_Base is Ada.Text_IO.Number_Base;
 
-   -- The generic X_Image functions return an image of Value, at least Width characters wide, in base Base,
+   -- The generic X_Image functions for integer types return an image of Value, at least Width characters wide, in base Base,
    -- padded with blanks or zeroes, according to Zero_Filled
 
    generic -- Signed_Image
@@ -28,11 +30,18 @@ package PragmARC.Images is
 
    generic -- Float_Image
       type Number is digits <>;
-   function Float_Image
-      (Value : Number; Fore : Field := 2; Aft : Field := Number'digits - 1; Exp : Field := 3; Zero_Filled : Boolean := False)
+   function Float_Image (Value       : Number;
+                         Fore        : Field       := 2;
+                         Aft         : Field       := Number'Digits - 1;
+                         Exp         : Field       := 3;
+                         Zero_Filled : Boolean     := False;
+                         Base        : Number_Base := 10)
    return String;
-   -- Returns an image of Value with at least Fore digits before the decimal point, padded with blanks or zeroes, according
-   -- to Zero_Filled. Fore, Aft, and Exp have the same meanings as in Ada.Text_IO.Float_IO.
+   -- If Base = 10, returns an image of Value with at least Fore digits before the decimal point, padded with blanks or zeroes,
+   -- accordingto Zero_Filled. Fore, Aft, and Exp have the same meanings as in Ada.Text_IO.Float_IO
+   -- If Base /= 10, Fore, Aft, and Exp are ignored, except that the result's length will be at least Fore + Aft + 1, and the image
+   -- of Value in base Base will as many digits as needed for the integer part, and no more that 1,000 digits for the fractional
+   -- part
 end PragmARC.Images;
 --
 -- This is free software; you can redistribute it and/or modify it under
