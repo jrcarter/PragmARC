9aa4737be3fa181cb59e3e96981c2d6253b8d327
Jeffrey R. Carter <16976909+jrcarter@users.noreply.github.com>
2021-01-29 23:25:59 +0100

    Correct error

--- pragmarc-min_max.ads
+++ pragmarc-min_max.ads
@@ -1,11 +1,12 @@
 -- PragmAda Reusable Component (PragmARC)
--- Copyright (C) 2020 by PragmAda Software Engineering.  All rights reserved.
+-- Copyright (C) 2021 by PragmAda Software Engineering.  All rights reserved.
 -- Released under the terms of the BSD 3-Clause license; see https://opensource.org/licenses
 -- **************************************************************************
 --
 -- Minimum and maximum functions for non-scalar values for which "<" is meaningful
 --
 -- History:
+-- 2021 Feb 01     J. Carter          V2.2--Expression functions illegal for limited type
 -- 2020 Dec 01     J. Carter          V2.1--Expression functions eliminate body; removed "="
 -- 2020 Nov 01     J. Carter          V2.0--Initial Ada-12 version
 ----------------------------------------------------------------------------
@@ -17,7 +18,7 @@ pragma Assertion_Policy (Check);
 pragma Unsuppress (All_Checks);
 
 generic -- PragmARC.Min_Max
-   type Element (<>) is limited private;
+   type Element (<>) is private;
 
    with function "<" (Left : Element; Right : Element) return Boolean is <>;
 package PragmARC.Min_Max with Pure is
