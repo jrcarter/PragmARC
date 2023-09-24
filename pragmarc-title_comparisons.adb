34a73bf53ed09a25a03c554d4a28f7dff1ccaae2
Jeffrey R. Carter <16976909+jrcarter@users.noreply.github.com>
2023-09-24 15:43:58 +0200

    Require space in article string

--- pragmarc-title_comparisons.adb
+++ pragmarc-title_comparisons.adb
@@ -6,14 +6,17 @@
 -- Compare two Strings in title order, ignoring an initial article
 --
 -- History:
+-- 2023 Sep 15     J. Carter          V1.1--Require space in article string
 -- 2022 Aug 01     J. Carter          V1.0--Initial version
 --
 with Ada.Characters.Handling;
 
 package body PragmARC.Title_Comparisons is
    English_Article : Article_List;
+   French_Article  : Article_List;
 
    function English return Article_List is (English_Article);
+   function French  return Article_List is (French_Article);
 
    function Munged (Item : in String; Article : in Article_List) return String;
    -- Moves any initial Article in Item to the end
@@ -25,8 +28,8 @@ package body PragmARC.Title_Comparisons is
          Extract : declare
             A : constant String := Ada.Characters.Handling.To_Basic (Ada.Characters.Handling.To_Lower (Article.Element (I) ) );
          begin -- Extract
-            if S'Length > A'Length + 1 and then S (S'First .. S'First + A'Length) = A & ' ' then
-               return S (S'First + A'Length + 1 .. S'Last) & ", " & A;
+            if S'Length > A'Length and then S (S'First .. S'First + A'Length - 1) = A then
+               return S (S'First + A'Length .. S'Last) & ", " & A;
             end if;
          end Extract;
       end loop Check;
@@ -40,7 +43,13 @@ package body PragmARC.Title_Comparisons is
    function Greater (Left : in String; Right : in String; Article : in Article_List := English) return Boolean is
       (Munged (Left, Article) > Munged (Right, Article) );
 begin -- PragmARC.Title_Comparisons
-   English_Article.Append (New_Item => "a");
-   English_Article.Append (New_Item => "an");
-   English_Article.Append (New_Item => "the");
+   English_Article.Append (New_Item => "a ");
+   English_Article.Append (New_Item => "an ");
+   English_Article.Append (New_Item => "the ");
+   French_Article.Append (New_Item => "un ");
+   French_Article.Append (New_Item => "une ");
+   French_Article.Append (New_Item => "le ");
+   French_Article.Append (New_Item => "la ");
+   French_Article.Append (New_Item => "les ");
+   French_Article.Append (New_Item => "l'");
 end PragmARC.Title_Comparisons;
--- pragmarc-title_comparisons.ads
+++ pragmarc-title_comparisons.ads
@@ -6,6 +6,7 @@
 -- Compare two Strings in title order, ignoring an initial article
 --
 -- History:
+-- 2023 Sep 15     J. Carter          V1.1--Require space in article string
 -- 2022 Aug 01     J. Carter          V1.0--Initial version
 --
 pragma Assertion_Policy (Check);
@@ -17,7 +18,10 @@ package PragmARC.Title_Comparisons is
    package Article_Lists is new Ada.Containers.Indefinite_Vectors (Index_Type => Positive, Element_Type => String);
    subtype Article_List is Article_Lists.Vector;
 
-   function English return Article_List; -- "a", "an", and "the"
+   function English return Article_List; -- "a ", "an ", and "the "
+   function French  return Article_List; -- "un ", "une ", "le ", "la ", "les ", and "l'"
+   -- Note that an article that is followed by a space must include the trailing space (English "a "), while an article that is not
+   -- followed by a space does not have one (French "l'")
 
    function Less    (Left : in String; Right : in String; Article : in Article_List := English) return Boolean;
    function Greater (Left : in String; Right : in String; Article : in Article_List := English) return Boolean;
