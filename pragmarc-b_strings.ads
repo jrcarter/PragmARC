e82a4d41043f4ae0c39bf2f7e02c921b2a434913
Daniel Norte de Moraes <danieagle@zohomail.com>
2025-01-12 17:34:19 -0300

    The old way working again with gcc14.2

--- pragmarc-b_strings.ads
+++ pragmarc-b_strings.ads
@@ -24,7 +24,7 @@ package PragmARC.B_Strings is
    type B_String (Max_Length : Positive := 1024) is tagged limited private;
    -- Default initial value is Null_B_String
 
-   Null_B_String : constant B_String (Max_Length => 1); -- A string of zero characters
+   Null_B_String : constant B_String; -- A string of zero characters
 
    function To_String (Source : in B_String) return String with
       Post => To_String'Result'First = 1 and To_String'Result'Last = Source.Length;
@@ -56,5 +56,5 @@ private -- PragmARC.B_Strings
       Value : String (1 .. Max_Length) := (1 .. Max_Length => ' ');
    end record;
 
-   Null_B_String : constant B_String (Max_Length => 1) := (others => <>);
+   Null_B_String : constant B_String := (Max_Length => 1, others => <>);
 end PragmARC.B_Strings;
